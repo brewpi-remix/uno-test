3b57a3618554522def25bfca63944d2aaa120fa3
Lee Bussy <lee@bussy.org>
2021-05-01 17:36:50 -0500

    Deconflict pin name

--- src/relaytest.cpp
+++ src/relaytest.cpp
@@ -1,51 +1,51 @@
 #include "relaytest.h"
 
-const char *pinName[] = {
+const char *relayPinName[] = {
     "Heat",
     "Cool"};
 
-const int pinNum[] = {
+const int relayPinNum[] = {
     HEATRELAY,
     COOLRELAY};
 
 void testRelays()
 {
-    for (int i = 0; i < (int)sizeof(pinNum) / (int)sizeof(pinNum[0]); i++)
+    for (int i = 0; i < (int)sizeof(relayPinNum) / (int)sizeof(relayPinNum[0]); i++)
     {
-        Log.notice(F(CR "Testing %s relay on pin %d." CR), pinName[i], pinNum[i]);
+        Log.notice(F(CR "Testing %s relay on pin %d." CR), relayPinName[i], relayPinNum[i]);
         if (hasLCD)
         {
             lcd.clear();
             lcd.setCursor(0, 0);
             lcd.print("Testing ");
-            lcd.print(pinName[i]);
+            lcd.print(relayPinName[i]);
             lcd.print(" relay");
             lcd.setCursor(0, 1);
             lcd.print("on pin ");
-            lcd.print(pinNum[i]);
+            lcd.print(relayPinNum[i]);
             lcd.print(": ");
         }
         _delay(1000);
-        pinMode(pinNum[i], OUTPUT);
-        digitalWrite(pinNum[i], RELAYOFF);
+        pinMode(relayPinNum[i], OUTPUT);
+        digitalWrite(relayPinNum[i], RELAYOFF);
         _delay(1000);
-        Log.notice(F("Turning %s relay on pin %d on." CR), pinName[i], pinNum[i]);
+        Log.notice(F("Turning %s relay on pin %d on." CR), relayPinName[i], relayPinNum[i]);
         if (hasLCD)
         {
             lcd.setCursor(10, 1);
             lcd.print("On");
         }
-        digitalWrite(pinNum[i], RELAYON);
+        digitalWrite(relayPinNum[i], RELAYON);
         _delay(2000);
-        Log.notice(F("Turning %s relay on pin %d off." CR), pinName[i], pinNum[i]);
+        Log.notice(F("Turning %s relay on pin %d off." CR), relayPinName[i], relayPinNum[i]);
         if (hasLCD)
         {
             lcd.setCursor(10, 1);
             lcd.print("Off");
         }
-        digitalWrite(pinNum[i], RELAYOFF);
+        digitalWrite(relayPinNum[i], RELAYOFF);
         _delay(2000);
-        Log.notice(F("%s relay test on pin %d complete." CR), pinName[i], pinNum[i]);
+        Log.notice(F("%s relay test on pin %d complete." CR), relayPinName[i], relayPinNum[i]);
        if (hasLCD)
         {
             lcd.setCursor(10, 1);
