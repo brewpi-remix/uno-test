39eb9e934331dec138934b7ad55ca7654db1ed47
Lee Bussy <lee@bussy.org>
2021-05-01 13:22:41 -0500

    Add display readout

--- src/relaytest.cpp
+++ src/relaytest.cpp
@@ -1,16 +1,56 @@
 #include "relaytest.h"
 
-void testRelay(int pin)
+const char *pinName[] = {
+    "Heat",
+    "Cool"};
+
+const int pinNum[] = {
+    HEATRELAY,
+    COOLRELAY};
+
+void testRelays()
 {
-    Log.notice(F(CR "Testing relay pin %d." CR), pin);
-    pinMode(pin, OUTPUT);
-    digitalWrite(pin, RELAYOFF);
-    delay(1000);
-    Log.notice(F("Turning relay on pin %d on." CR), pin);
-    digitalWrite(pin, RELAYON);
-    delay(1000);
-    Log.notice(F("Turning relay on pin %d off." CR), pin);
-    delay(250);
-    digitalWrite(pin, RELAYOFF);
-    Log.notice(F("Relay test on pin %d complete." CR), pin);
+    for (int i = 0; i < (int)sizeof(pinNum) / (int)sizeof(pinNum[0]); i++)
+    {
+        Log.notice(F(CR "Testing %s relay on pin %d." CR), pinName[i], pinNum[i]);
+        if (hasLCD)
+        {
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print("Testing ");
+            lcd.print(pinName[i]);
+            lcd.print(" relay");
+            lcd.setCursor(0, 1);
+            lcd.print("on pin ");
+            lcd.print(pinNum[i]);
+            lcd.print(": ");
+        }
+        _delay(1000);
+        pinMode(pinNum[i], OUTPUT);
+        digitalWrite(pinNum[i], RELAYOFF);
+        _delay(1000);
+        Log.notice(F("Turning %s relay on pin %d on." CR), pinName[i], pinNum[i]);
+        if (hasLCD)
+        {
+            lcd.setCursor(10, 1);
+            lcd.print("On");
+        }
+        digitalWrite(pinNum[i], RELAYON);
+        _delay(2000);
+        Log.notice(F("Turning %s relay on pin %d off." CR), pinName[i], pinNum[i]);
+        if (hasLCD)
+        {
+            lcd.setCursor(10, 1);
+            lcd.print("Off");
+        }
+        digitalWrite(pinNum[i], RELAYOFF);
+        _delay(2000);
+        Log.notice(F("%s relay test on pin %d complete." CR), pinName[i], pinNum[i]);
+       if (hasLCD)
+        {
+            lcd.setCursor(10, 1);
+            lcd.print("Complete");
+        }
+        _delay(2000);
+    }
 }
--- src/relaytest.h
+++ src/relaytest.h
@@ -1,11 +1,19 @@
 #ifndef _RELAYTEST_H
 #define _RELAYTEST_H
 
+#include "tools.h"
+#include "lcd.h"
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 #include <ArduinoLog.h>
 #include <Arduino.h>
 
 #include "config.h" // Should be after Arduino.h
 
-void testRelay(int pin);
+void testRelays();
+
+extern bool hasLCD;
+extern byte lcdAddress;
+extern LiquidCrystal_I2C lcd;
 
 #endif // _RELAYTEST_H
